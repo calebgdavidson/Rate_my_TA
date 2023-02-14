a368f654b0161d916d6bfcc6f74962f8c540a527
zMag33z <mag33.cdh@gmail.com>
2023-02-13 21:00:19 -0600

    models added to model folder

---
+++ models/Comment.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        comment_text: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        comment_date: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        commenter_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'post',
+                key: 'id',
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    }
+);
+
+module.exports = Comment;
---
+++ models/index.js
@@ -0,0 +1,33 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+Post.belongsTo(User, {
+    foreignKey: 'poster_id',
+});
+
+User.hasMany(Post, {
+    foreignKey: 'poster_id',
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'commenter_id',
+});
+
+Comment.belongsTo(Post, {
+    foreignKey:  'post_id',
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'commenter_id',
+});
+
+module.exports = {
+    User,
+    Post,
+    Comment
+};
