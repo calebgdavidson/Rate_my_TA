6b06dfcea5a26ec6800bbba8ec12c6077f83fe81
victorlmorales <victor.l.morales2020@gmail.com>
2023-02-15 22:10:01 -0600

    Fixed routing for app to use postRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,10 @@
 const api = require('express').Router();
 const userRoutes = require('./userRoutes');
-const postRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
 const commentRoutes = require('./userRoutes');
 const { withAuth, hasAuth } = require('../../utils/auth');
 
 api.use('/users', userRoutes);
+api.use('/posts', postRoutes);
 
 module.exports = api;
